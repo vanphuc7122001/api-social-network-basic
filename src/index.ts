cc6d4e78e95a1376b2752840d5bc5eed64880225
Phuc lucas <dangvanphuc7122001@gmail.com>
2023-12-07 21:30:25 +0700

    Limit request to server in 15 minute 100 request

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors, { CorsOptions } from 'cors'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 import { initRoutes } from './routes/index.routes'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import databaseService from './services/database.service'
@@ -19,9 +20,18 @@ const corsOptions: CorsOptions = {
   methods: 'GET, POST, PUT, DELETE, PATH'
 }
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+})
+
 initFolder()
 app.use(cors(corsOptions))
 app.use(helmet())
+app.use(limiter)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/static/videos', express.static(UPLOAD_VIDEOS_DIR))
